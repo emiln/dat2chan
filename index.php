a9e0b1f440731f68e0e90ff3795718283498ea1a
Emil Nauerby <emil@nauerby.com>
2012-05-16 06:11:16 +0200

    Switched to a switched statement. Aww yeah.

--- index.php
+++ index.php
@@ -3,12 +3,21 @@
 $params = explode('/', $_GET['url']);
 
 // Determine whether a specific page was requested. If not, send default page.
-if (count($params) > 1) {
-    echo 'You requested a board called "' . $params[0] . '" and an ID=' .
-        $params[1];
-} elseif (count($params) == 1 && !empty($params[0])) {
-    echo 'You requested the front page of the board called "' . $params[0] .
-        '".';
-} else {
-    echo 'You did not request a board at all. What are you doing, bro?';
+switch (count($params)) {
+    case 0:
+        echo 'You requested nothing at all. What are you doing, bro?';
+        break;
+    case 1:
+        echo empty($params[0]) ? 'You requested an empty board.' :
+            'You requested the front page of board "' . $params[0] . '".';
+        break;
+    case 2:
+        echo 'You requested a thread called "' . $params[1] . '" from a board '
+            . 'called "' . $params[0] . '".';
+        break;
+    case 3:
+        echo $params[1] == 'page' ? 'You requested page ' . $params[2] .
+            ' of the board "' . $params[0] . '"."' :
+            'I am not really sure what you requested, bro.';
+        break;
 }
