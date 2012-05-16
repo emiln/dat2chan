e647a1539c2b9ebfe9ff97e6aae0f01f68bcdec2
Emil Nauerby <emil@nauerby.com>
2012-05-16 05:47:38 +0200

    Now with amazing error handling.

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 $params = explode('/', $_GET['url']);
 
 // Determine whether a specific page was requested. If not, send default page.
-if (count($params) == 0 || empty($params[0])) {
-    // Default page.
+if (count($params) > 1) {
+    echo 'You requested a board called "' . $params[0] . '" and an ID=' .
+        $params[1];
+} elseif (count($params) == 1 && !empty($params[0])) {
+    echo 'You requested the front page of the board called "' . $params[0] .
+        '".';
 } else {
-    // Specific board page.
+    echo 'You did not request a board at all. What are you doing, bro?';
 }
-
-echo 'You requested the URL "' . $_GET['url'] . '".';
