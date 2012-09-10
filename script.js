77fdab449eeb3c8b59cc7d31994658d8d7951de1
Emil Nauerby <emil@nauerby.com>
2012-09-10 21:41:59 +0200

    Now blurs (unselects) the anchor after clicking it. Looks cleaner.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#hide-watcher').toggle(
         function() {
+            $('#hide-watcher > a').blur();
             $('#watchlist, #searchlist').hide(250);
             if (!$('#right-menu').attr('old-height')) {
                 var h = $('#right-menu').height();
@@ -8,10 +9,11 @@ $(document).ready(function() {
             }
             $('#hide-watcher > a').text('Show');
             $('#right-menu').animate({
-                width: '35px',
-                height: '20px'
+                width: '42px',
+                height: '22px'
             }, 250);
         }, function() {
+            $('#hide-watcher > a').blur();
             $('#watchlist, #searchlist').show(250);
             var h = $('#right-menu').attr('old-height');
             $('#hide-watcher > a').text('Hide');
