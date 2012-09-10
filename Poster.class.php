b04a31ff3c5892d830973f3fe78346a571686bb4
Torgeir Johansen <torgeir.johansen92@gmail.com>
2012-09-10 17:21:51 +0200

    Added a few lines of code (pun very much intended)

--- Poster.class.php
+++ Poster.class.php
@@ -3,8 +3,8 @@ class Poster {
     static function thread($fields) {
         extract($fields);
         echo '<div class="thread">';
-        echo "<h2><span class=\"op\">$op</span> $title " .
-          "<span class=\"time\">$time</span></h2>";
+        echo "<h2><span class=\"op\">$op</span> - $title " .
+          "- <span class=\"time\">$time</span></h2>";
         echo '<div class="message">' . nl2br($message) . '</div>';
         echo "</div>\n";
     }
