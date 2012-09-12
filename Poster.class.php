dc257cce499666ac7cf66cac4da48c10e0a79c57
Emil Nauerby <emil@nauerby.com>
2012-09-12 12:52:46 +0200

    Rewrote some PHP to make it more readable and fixed a non-validating part of the header. No text in <ol>, it seems.

--- Poster.class.php
+++ Poster.class.php
@@ -2,24 +2,36 @@
 class Poster {
     static function thread($fields) {
         extract($fields);
-        echo '<div class="thread">';
-        echo "<h2><span class=\"op\">$op</span>$title" .
-          "<span class=\"time\">$time</span></h2>";
-        echo '<div class="message">' . nl2br($message) . '</div>';
-        echo "</div>\n";
+?>
+<div class="thread">
+  <h2>
+    <span class="op"><?php echo $op; ?></span>
+    <?php echo $title; ?>
+    <span class="time"><?php echo $time; ?></span>
+  </h2>
+  <div class="message"><?php echo nl2br($message); ?></div>
+</div>
+<?php
     }
 
     static function no_threads() {
-        echo '<h2>No threads found for this board.</h2>';
-        echo '<p>This would be a great time to create one, I bet.</p>';
+?>
+<h2>No threads found for this board.</h2>
+<p>This would be a great time to create one, I bet.</p>
+<?php
     }
 
     static function reply($reply) {
         extract($reply);
-        echo '<div class="reply">';
-        echo "<h3><span class=\"poster\">$poster</span>$title" .
-          "<span class=\"time\">$time</span></h3>";
-        echo '<div class="reply-message">' . nl2br($message) . '</div>';
-        echo "</div>\n";
+?>
+<div class="reply">
+  <h3>
+    <span class="poster"><?php echo $poster; ?></span>
+    <?php echo $title; ?>
+    <span class="time"><?php echo $time; ?></span>
+  </h3>
+  <div class="reply-message"><?php echo nl2br($message); ?></div>
+</div>
+<?php
     }
 }
